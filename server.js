fc0c377e0a890dabc4c04faaba05956f928d1578
zakharov <zakharov@signatec.ru>
2017-09-08 12:47:56 +0700

    auth callback

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ app.use(bodyParser.json());
 app.set('port', process.env.PORT || 8080);
 app.use(cors()); // CORS (Cross-Origin Resource Sharing) headers to support Cross-site HTTP requests
 app.use(express.static("dist")); // Our Ionic app build is in the www folder (kept up-to-date by the Ionic CLI using 'ionic serve')
+app.use("/callback", express.static("dist")); // Our Ionic app build is in the www folder (kept up-to-date by the Ionic CLI using 'ionic serve')
 
 var DATABASE_URL = process.env.DATABASE_URL
   || 'postgres://developer:dev@localhost:5432/chrono_dev_db';
