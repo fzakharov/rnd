9b31a17455951e5a475b56ef69cf25973737e0a6
fzakharov <seesharp@mail.ru>
2017-09-13 22:27:08 +0700

    spinner

--- qvue/src/components/Callback.vue
+++ qvue/src/components/Callback.vue
@@ -1,31 +1,20 @@
 <template>
-  <div class="spinner">
-    <img src="../assets/loading.svg" alt="loading"/>
+  <div>
+    <q-spinner-mat size="50px" color="primary"></q-spinner-mat>
   </div>
 </template>
 
 <script>
-  export default {
-    name: 'callback',
-    props: ['auth'],
-    data () {
-      this.auth.handleAuthentication()
-      return {}
-    }
+import { QSpinnerMat } from 'quasar'
+export default {
+  components: {
+    QSpinnerMat
+  },
+  name: 'callback',
+  props: ['auth'],
+  data () {
+    this.auth.handleAuthentication()
+    return {}
   }
+}
 </script>
-
-<style>
-  .spinner {
-    position: absolute;
-    display: flex;
-    justify-content: center;
-    height: 100vh;
-    width: 100vw;
-    background-color: white;
-    top: 0;
-    bottom: 0;
-    left: 0;
-    right: 0;
-  }
-</style>
